aee47aaeb0f31d5a7da58b4dd8eb35bf486c489d
CER-ArnauRP <a.rodriguez@ceroca.cat>
2025-10-06 11:14:01 +0200

    Pantalla principal: navegació entre pestanyes superiors

--- lib/pagines/pagina_principal.dart
+++ lib/pagines/pagina_principal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/colors_app.dart';
 import 'package:myapp/components/search_delegate_cerca_principal.dart';
 import 'package:myapp/components/titol_app.dart';
 import 'package:myapp/data/dades_locals.dart';
@@ -10,19 +11,52 @@ class PaginaPrincipal extends StatefulWidget {
   State<PaginaPrincipal> createState() => _PaginaPrincipalState();
 }
 
-class _PaginaPrincipalState extends State<PaginaPrincipal> {
+class _PaginaPrincipalState extends State<PaginaPrincipal> with SingleTickerProviderStateMixin {
+  // El with SingleTickerProviderStateMixin és pels tabs. Si no es posa, la línia 
+  //    controllerPestanya = TabController(length: 3, vsync: this); dona error.
+
+  // Use SingleTickerProviderStateMixin for a single TabController. If you need multiple 
+  //    TabControllers or animations, use TickerProviderStateMixin instead.
 
   // Variable pel cercador/filtre.
   List<Map<String, String>> platsFiltrats = DadesLocals.llistaPlats.cast<Map<String, String>>();
+
+  late TabController controllerPestanya;
+  // "late": quan una variable no pot valdre null, però no es pot inicialitzar 
+  //    fins que l'aplicació es posa en marxa.
+
+  @override
+  void initState() {
+    
+    super.initState();
+
+    controllerPestanya = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    
+    controllerPestanya.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
+        // Títol i característiques generals de la AppBar.
+        // ===============================================
         title: TitolApp(text1: "Savor", text2: "Sphere", midaGran: false,),
-        centerTitle: true,
+        //centerTitle: false,
+        backgroundColor: ColorsApp.colorSecundariTransperencia,
+
+        // Botons d'accions de la AppBar (només en posem un, el botó de cerca).
+        // ====================================================================
         actions: [
 
+          // Botó de cerca.
           IconButton(
             icon: Icon(Icons.search), 
             onPressed: () async {
@@ -35,7 +69,8 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
                 delegate: SearchDelegateCercaPrincipal()
               );
 
-              // Una vegada s'hagi pogut crear l'objecte (el retorna un mètode await).
+              // Una vegada s'hagi pogut crear l'objecte (el retorna un mètode await), 
+              //    comprobem que no sigui null.
               if (itemsSeleccionats != null) { 
                 setState(() {
                   platsFiltrats = [itemsSeleccionats];
@@ -52,13 +87,43 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
           ),
         ],
         
+        // Creem les pestanyes superiors de la AppBar.
+        // ===========================================
         // Amb la AppBar, es poden posar els ítems de la barra de dalt.
-        //    però també es poden posar els de la barra de baix, amb 
-        //    l'atribut "bottom".
+        //    entre els quals hi ha una barra de navegació de la part 
+        //    superior de la pantalla, amb l'atribut "bottom" (a sota 
+        //    de la barra de navegació).
+        bottom: TabBar(
+          controller: controllerPestanya,
+          labelColor: ColorsApp.colorPrimariAccent2,
+          unselectedLabelColor: ColorsApp.colorSecundariAccent,
+          //indicator: ColorsApp.colorPrimariAccent,
+          tabs: [
+            Tab(icon: Icon(Icons.food_bank), text: "General",),
+            Tab(icon: Icon(Icons.local_pizza), text: "Italià",),
+            Tab(icon: Icon(Icons.rice_bowl), text: "Asiàtic",),
+          ],
+        ),
       ),
+
+      // Contingut de la pantalla principal.
+      // ===================================
+      // (el contingut és la pestanya que estigui seleccionada).
 
       // En el body del Scaffold, li passem un TabBarView (i definim les 
       //    pàgines a les que permeti navegar).
+      body: TabBarView(
+        controller: controllerPestanya,
+        children: [// Posar-hi les pàgines que volguem.
+          _pestanyaGeneral(),
+          Center(child: Text("Pestanya 2"),),
+          Center(child: Text("Pestanya 3"),),
+        ],
+      ),
     );
   }
+}
+
+Widget _pestanyaGeneral() {
+  return Container(child: Text("Hola"),);
 }
